43469d9c7cb093376360f0a0d9078c581eb6cd77
Dylan James <djame011@gold.ac.uk>
2021-11-09 15:20:13 +0000

    step 7 completed

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,7 +1,16 @@
 require 'bookmark'
 
 describe Bookmark do
+
   describe '.all' do
+
+    # it 'updates the #all class method' do
+    # connection = PG.connect(dbname: 'bookmark_manager')
+    # result = connection.exec('SELECT * FROM bookmarks')
+
+    # expect(bookmarks).to include("http://www.makersacademy.com")
+    # end
+
     it 'returns all the bookmarks' do
       bookmarks = Bookmark.all
 
@@ -9,5 +18,7 @@ describe Bookmark do
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
     end
+
   end
+
 end

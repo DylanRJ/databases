887cadae27b8b23d9a90de3b6249cbc4eb4937f4
Dylan James <djame011@gold.ac.uk>
2021-11-11 14:57:52 +0000

    Chapter 11 completed

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -2,8 +2,9 @@ feature 'adding a bookmark' do
   scenario 'submitting bookmark and seeing it displayed' do
     visit('/add_bookmark')
     fill_in('url', with: 'www.gov.org.uk')
+    fill_in('title', with: 'Gov')
     click_button('Submit')
     
-    expect(page).to have_content 'www.gov.org.uk'
+    expect(page).to have_link('Gov', href: 'www.gov.org.uk')
   end
 end
